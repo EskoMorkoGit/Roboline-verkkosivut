f770ff62ab09e41125baa7062265f1df88964023
EskoMorkoGit <eskomk@gmail.com>
2026-05-18 14:01:38 +0300

    Update

---
+++ temp.js
@@ -0,0 +1,22 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            brand: {
+              primary: '#0077b8', // Based on THEME_2 custom color
+              dark: '#1a202c',
+              light: '#f8fafc',
+            }
+          },
+          fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            display: ['Montserrat', 'sans-serif'],
+          },
+          borderRadius: {
+            'custom': '8px',
+          }
+        }
+      }
+    }
+  
